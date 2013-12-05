2bcb56c67a98c6931116c3872822e3df1f6b3059
JLangowitz <joshua.langowitz@students.olin.edu>
2013-12-05 17:06:34 -0500

    link to other website, error handling not working yet

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,14 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+};
+
+exports.upload = function(req, res){
+    try{
+        res.redirect('http://autoschematic.ngrok.com/upload')
+    }
+    catch(err){
+    res.render('upload', { title: 'Sorry' });
+    }
 };
