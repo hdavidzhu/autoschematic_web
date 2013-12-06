1eb7fb378fd02e99a460df3a32e66e77d608f240
JLangowitz <joshua.langowitz@students.olin.edu>
2013-12-05 19:36:42 -0500

    fixed linking

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ exports.upload = function(req, res){
     var url = 'http://autoschematic.ngrok.com/upload'
     request(url, function(err, resp, body){
         if (err || body.match(/tunnel/i)){
-            res.render('upload', { title: 'Sorry', url:'http://placekitten.com/'+(Math.floor(Math.random()*(401)+200)+'/'+(Math.floor(Math.random()*(401)+200) });
+            res.render('upload', { title: 'Sorry', url:'http://placekitten.com/'+(Math.floor(Math.random()*(401)+200))+'/'+(Math.floor(Math.random()*(401)+200)) });
         }
         else{
             res.redirect('http://autoschematic.ngrok.com/upload');
